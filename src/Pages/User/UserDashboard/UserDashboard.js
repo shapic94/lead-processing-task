da4a44f5d0c4560eb95c8a14558bddaaa3895dfc
nebojsasapicogury <nebojsa.sapic@ogury.co>
2021-05-22 18:26:51 +0200

    Change on ws initialization and removing build folder from git tracking

--- src/Pages/User/UserDashboard/UserDashboard.js
+++ src/Pages/User/UserDashboard/UserDashboard.js
@@ -3,30 +3,35 @@ import {connect} from 'react-redux';
 import {w3cwebsocket as W3CWebSocket} from "websocket";
 import EmailComponent from "../../../Components/Email/Email";
 import NavigationComponent from "../../../Components/Navigation/Navigation";
-
-import './UserDashboard.scss';
 import LoadingComponent from "../../../Components/Loading/Loading";
 import {setEmail, setUser} from "../../../Redux/Actions";
 import IdleComponent from "../../../Components/Idle/Idle";
 import EmailActionsComponent from "../../../Components/EmailActions/EmailActions";
 
+import './UserDashboard.scss';
+
 class UserDashboardPage extends Component {
 	constructor() {
 		super();
 		this.state = {
-			client: new W3CWebSocket('wss://8wyzugxnaf.execute-api.us-east-2.amazonaws.com/production')
+			client: null
 		}
 	}
+
 	componentWillMount() {
-		console.log('uslo')
+		this.setState({
+			client: new W3CWebSocket('wss://8wyzugxnaf.execute-api.us-east-2.amazonaws.com/production')
+		})
+
+	}
+
+	componentDidMount() {
 		this.state.client.onopen = () => {
-			console.log('online')
 			this.props.setUser({...this.props.user, status: 'online'})
 		};
 
 		this.state.client.onmessage = (message) => {
 			let data = JSON.parse(message.data);
-
 
 
 			if ('status' in data) {
@@ -37,15 +42,22 @@ class UserDashboardPage extends Component {
 		};
 
 		this.state.client.onclose = () => {
-			console.log('offline')
 			this.props.setUser({...this.props.user, status: 'offline'})
 		};
+	}
+
+	componentWillUnmount() {
+		this.state.client.close();
 	}
 
 	componentDidUpdate(prevProps, prevState, snapshot) {
 		if (prevProps.user && this.props.user && prevProps.user.status !== this.props.user.status) {
 			if (this.state.client.readyState === this.state.client.OPEN) {
-				this.state.client.send(JSON.stringify({type: 'USER_STATUS', user: this.props.user, email: this.props.email}));
+				this.state.client.send(JSON.stringify({
+					type: 'USER_STATUS',
+					user: this.props.user,
+					email: this.props.email
+				}));
 
 				if (this.props.user.status === 'away') {
 					this.props.setEmail(null);
